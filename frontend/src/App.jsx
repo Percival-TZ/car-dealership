149c934506f99795b12c606ed4a284c4c52827f5
Percival-TZ <ellylous093@gmail.com>
2026-05-31 14:34:23 +0300

    frontend modification

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
-import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+import Login from "./pages/Login.jsx";
+import Favorites from "./pages/Favourates.jsx";
+import Register from "./pages/Register.jsx";
+import AdminDashboard from "./pages/AdminDashboard.jsx";
+import Home from "./pages/Home.jsx";
 
 function App() {
-  return <Home />;
+    return (
+        <>
+            <Login />
+            <Favorites />
+            <Register />
+            <AdminDashboard />
+            <Home />
+        </>
+    );
 }
 
 export default App;
